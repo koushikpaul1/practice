688f6e6b1a157c29f99114968e652f2fbf773390
koushikpaul <koushikpaul1@gmail.com>
2016-04-08 02:48:04 +0530

    Removing extra files

--- adhoop-master/anagram/run.sh
+++
@@ -1,6 +0,0 @@
-javac -classpath $HADOOP_HOME/hadoop-core.jar *.java
-jar cvf anagram.jar *.class
-hadoop fs -rmr outputAnagram
-hadoop jar anagram.jar AnagramDriver inputAnagram outputAnagram
-rm -f output.txt
-hadoop fs -cat 'outputAnagram/part-*' > output.txt
--- adhoop-master/matrix_multiplication_2/run.sh
+++
@@ -1,7 +0,0 @@
-rm -f *.class
-rm -f *.jar
-
-javac -classpath /usr/lib/hadoop/hadoop-core.jar *.java
-jar cvf matrix_multiplication_one.jar *.class
-
-hadoop jar matrix_multiplication_one.jar MatrixMultiplicationOneDriver M.txt N.txt
